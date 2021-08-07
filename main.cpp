42f97f00b81265e2fc18f548083e1e40908bcdb1
amirhosein13 <amirhoseinfar1385@gmail.com>
2021-08-07 11:36:42 -0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int m,n;
+    cin>>m>>n;
+    int a=m*n;
+    if (a%2==0)
+    {
+        cout << a/2<<endl;
+    }
+    else{
+        cout << (a-1)/2<<endl;
+    }
+    
+}
